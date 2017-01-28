88be5186e5cb4aceda08ef60e6c34472e47b3a95
ashish-agarwal <ashishcd94@gmail.com>
2017-01-28 16:58:54 +0530

    signup view and functionality added

--- blog.py
+++ blog.py
@@ -4,6 +4,8 @@ import os
 import webapp2
 import jinja2
 import hmac
+import re
+
 # Importing db models
 from models.user import User
 from models.post import Post
@@ -71,19 +73,6 @@ class MainPage(BlogHandler):
     print "posts",posts
     # self.write(posts)      
     self.render('front-page.html',posts = posts)
-
-class Register(BlogHandler):
-  def get(self):
-    f = User.register("ads","ADS","")
-    f.put()
-    self.login(f)     
-    self.write("hello")
-
-  def post(self):
-    u = User.register(self.request.get('username'), self.request.get('password'), self.request.get('email'))
-    u.put()
-    self.login(u)     
-    self.write("signed up successfully and also cookie is set")
     
 class Login(BlogHandler):
     def post(self):
@@ -135,11 +124,71 @@ class PostHandler(BlogHandler):
         print post
         self.render("post.html",p = post)
 
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+class Register(BlogHandler):
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.verify = self.request.get('verify')
+        self.email = self.request.get('email')
+
+        params = dict(username = self.username,
+                      email = self.email)
+                      
+        if not valid_username(self.username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        u = User.by_name(self.username)
+
+        if u:
+            params['error_username'] = 'That user already exists.'
+            have_error = True
+
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+
+            self.login(u)
+            self.redirect('/')
+
+
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/register',Register),
                                ('/login',Login),
                                ('/logout',Logout),
                                ('/newpost', NewPost),
+                               ('/signup', Register),
                                ('/blog/(\d+)',PostHandler)
                                ],
                               debug=True)
